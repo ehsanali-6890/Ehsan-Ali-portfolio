6c68fb4fdc67f3b23a319fae1895cc58242af14e
ehsanali-6890 <ehsanali6890@gmail.com>
2026-06-14 13:00:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".counter");
 
+  counters.forEach(counter => {
+    const target = +counter.getAttribute("data-target");
+    let count = 0;
+
+    const update = () => {
+      const increment = target / 80;
+
+      if (count < target) {
+        count += increment;
+        counter.innerText = Math.floor(count);
+        requestAnimationFrame(update);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    update();
+  });
+});
